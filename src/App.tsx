e49c5ecf475a1874399cb10a078c21a9df94eb97
Mihailo <mihailoless@gmail.com>
2026-01-07 21:21:30 +0100

    Starting over.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { DropdownButton } from "./components/DropdownButton/DropdownButton"
-import { dificultyItems, timeItems } from "./features/typingTest/data"
+
 function App() {
 
   return (
-    <div className="flex gap-5">
-      <DropdownButton
-        items={timeItems}
-        label={'Time'}
-      />
-      <DropdownButton
-        items={dificultyItems}
-        label={'Mode'}
-      />
-    </div>
+    <>
+    </>
   )
 }
 
--- src/types/dropdown.ts
+++
@@ -1,4 +0,0 @@
-export type DropdownItemProps<T> = {
-  value: T;
-  label: string;
-}
