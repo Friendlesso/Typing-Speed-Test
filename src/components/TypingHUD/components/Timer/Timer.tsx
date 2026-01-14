78e348b4d21a8a936317e0d4a30e6e769b837608
Mihailo <mihailoless@gmail.com>
2026-01-14 18:35:47 +0100

    Fixed styling issues in Timer.tsx

--- src/components/TypingHUD/components/Timer/Timer.tsx
+++ src/components/TypingHUD/components/Timer/Timer.tsx
@@ -33,8 +33,8 @@ export function Timer({ time, isStarted, setIsStarted }: TimerProps) {
 
   return (
     <div className="border-r-2 border-(--neutral-700) px-3">
-      <p className="text-(--neutral-400) text-xl">Time:
-        <span className={`${isStarted ? 'text-(--yellow-400)' : 'text-white'} text-2xl font-bold pl-1`}>
+      <p className="text-(--neutral-400) text-xl/[120%] -tracking-[0.0375rem]">Time:
+        <span className={`${isStarted ? 'text-(--yellow-400)' : 'text-white'} text-2xl font-bold pl-3`}>
           {formattedTime}
         </span>
       </p>
